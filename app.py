2ff3d16cdd90e40bcce1caedd1cd8f8392c1fb8b
Ajay Biswas <104336525+BlueSquare4@users.noreply.github.com>
2024-10-25 16:24:43 +0530

    removed contact icon

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ import re
 # Create Flask app
 app = Flask(__name__)
 
-# Initialize Firebase Admin SDK
-cred = credentials.Certificate("portfolio-2f0e2-firebase-adminsdk-ri2yx-1b0dd47dda.json")
-firebase_admin.initialize_app(cred)
-db = firestore.client()
+# # Initialize Firebase Admin SDK
+# cred = credentials.Certificate("portfolio-2f0e2-firebase-adminsdk-ri2yx-1b0dd47dda.json")
+# firebase_admin.initialize_app(cred)
+# db = firestore.client()
 
 # Function to add contact to Firestore
-def add_contact(name, phone, email, reason):
-    doc_ref = db.collection(u'contacts').document()
-    doc_ref.set({
-        u'name': name,
-        u'phone': phone,
-        u'email': email,
-        u'reason': reason,
-        u'date': datetime.now()
-    })
+# def add_contact(name, phone, email, reason):
+#     doc_ref = db.collection(u'contacts').document()
+#     doc_ref.set({
+#         u'name': name,
+#         u'phone': phone,
+#         u'email': email,
+#         u'reason': reason,
+#         u'date': datetime.now()
+#     })
 
 
 # def get_projects():
@@ -63,20 +63,20 @@ def home():
 def contact_page():
     contact_status = None
 
-    if request.method == 'POST':
-        name = request.form.get('name', '').strip()
-        email = request.form.get('email', '').strip()
-        phone = request.form.get('phone', '').strip()
-        reason = request.form.get('reason', '').strip()
+    # if request.method == 'POST':
+    #     name = request.form.get('name', '').strip()
+    #     email = request.form.get('email', '').strip()
+    #     phone = request.form.get('phone', '').strip()
+    #     reason = request.form.get('reason', '').strip()
 
-        # Check phone number
-        if 10 <= len(phone) <= 13 and re.fullmatch(r'^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$', phone):
-            add_contact(name, phone, email, reason)
-            contact_status = True
-        else:
-            contact_status = False
+        # # Check phone number
+        # if 10 <= len(phone) <= 13 and re.fullmatch(r'^([+]?[\s0-9]+)?(\d{3}|[(]?[0-9]+[)])?([-]?[\s]?[0-9])+$', phone):
+        #     add_contact(name, phone, email, reason)
+        #     contact_status = True
+        # else:
+        #     contact_status = False
 
-    return render_template('contact.html', title='Contact Page', contact_status=contact_status)
+    return render_template('projects.html', title='Contact Page', contact_status=contact_status)
 
 
 @app.route('/projects')
